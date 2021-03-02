d63bb14295bc085525b5660db5a636bfb908d0e8
reza <rezaaa555555r@gmail.com>
2021-03-02 20:56:10 +0330

    upload

---
+++ src/App.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="row" id="app">
+    <div
+      class="col-3 col-md-2 col-lg-1 mt-2 border-right d-flex flex-column align-items-center"
+    >
+      <p class="lead">Docket</p>
+      <button
+        class="d-flex align-items-center justify-content-center"
+        id="add-note"
+        @click="addNewNote"
+      >
+        <img alt="" src="icons/plus.png" style="max-width: 90%" />
+      </button>
+    </div>
+    <div class="col-7 col-md-8 col-lg-10 mt-2 ms-2">
+      <div>
+        <form>
+          <div class="my-0">
+            <label class="col-form-label" for="search-note"></label>
+            <input
+              class="border-0 form-control shadow-none"
+              id="search-note"
+              placeholder="Search..."
+              type="text"
+              @keyup="searchNote"
+              v-model="searchNoteValue"
+            />
+          </div>
+        </form>
+        <h1 class="mt-3">Notes</h1>
+      </div>
+      <div class="row text-center justify-content-center note-container">
+        <noteApp
+          v-for="note in Notes"
+          :key="note.id"
+          :noteObj="note"
+          :newNote="note.text ? true : false"
+          @edit-note="editNote"
+          @del-note="delNote"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import noteApp from "./components/noteApp.vue";
+
+export default {
+  name: "App",
+  components: {
+    noteApp,
+  },
+  data() {
+    return {
+      Notes: (JSON.parse(localStorage.getItem("note") ?? null) || []).filter(e=>{if(!(!e.text&&!e.title)){return e}}),
+      searchNotes:[],
+      searchNoteValue:''
+    };
+  },
+  methods: {
+    addNewNote: function () {
+      this.Notes.unshift({
+        title: "",
+        text: "",
+        time: this.getDate().time,
+        date: this.getDate().Date,
+        id: Math.random()*100000,
+      });
+      this._AddlocalStorage();
+    },
+    editNote: function(e){
+      this.Notes.map(note=>{
+        if(note.id === e.id){note=e;this._AddlocalStorage()}
+      })
+    },
+    delNote: function (e) {
+      this.Notes = this.Notes.filter(note=>{
+        if(note.id !== e.id){return note}
+      })
+      this._AddlocalStorage();
+    },
+    _AddlocalStorage: function () {
+      localStorage.setItem("note", JSON.stringify(this.Notes));
+    },
+    searchNote: function () {
+      if(this.searchNoteValue){
+        this.searchNotes = this.Notes
+        this.Notes = this.Notes.filter(e=>{
+          if(e.title.includes(this.searchNoteValue)){
+            return e
+          }
+        })
+      }else{
+          this.Notes = this.searchNotes
+      }
+    },
+    getDate: function(){
+        const now = new Date();
+        const options = {
+          month: 'short',
+          day: 'numeric',
+          year: 'numeric',
+        };
+        const hour = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const time = `${hour}:${minutes}`;
+        const formattedDate = new Intl.DateTimeFormat('en-US', options).format(now);
+        return {
+          Date:formattedDate,
+          time:time
+        }
+    }
+  },
+};
+</script>
+
+
+
---
+++ src/components/noteApp.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="card col-md-6 mx-2 my-2 note transition" :class="{showNoteMode : (showNoteMode && newNoteBl)}">
+    <div class="card-body">
+      <div class="d-flex justify-content-center">
+        <button @click="delNote" class="btn m-0 p-0 shadow-none delete-note">
+          <img alt="" class="m-0 p-0" src="icons/close.png" style="max-width: 45%" />
+        </button>
+        <input :readonly="(showNoteMode && newNoteBl)" class="card-title mt-0 text-center note-title" maxlength="17" type="text" v-model="noteObject.title" />
+      </div>
+      <label for="content"></label>
+      <textarea class="note-text" cols="30" :readonly="(showNoteMode && newNoteBl)" dir="auto" id="content" rows="8" v-model="noteObject.text"></textarea>
+      <div class="d-flex justify-content-between align-items-center mb-4 mt-2">
+        <div class="d-flex flex-column justify-content-center">
+          <p class="m-0 note-date">{{ noteObject.date }}</p>
+          <small class="note-time">{{ noteObject.time }}</small>
+        </div>
+        <button class="btn shadow-none m-0 p-0 btn-note d-flex justify-content-center align-items-center">
+          <img  @click="editNote" alt="" class="d-none edit-btn" src="icons/pen.png" style="max-width: 60%" />
+          <img  @click="saveNote" src="icons/check.png" class="check-btn mw-100" alt="" />
+        </button>
+      </div>
+      </div>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+  name: "noteApp",
+  props:{
+    noteObj: Object,
+    newNote: Boolean,
+  },
+  data: function (){
+    return{
+      editedNote:{},
+      noteObject:this.noteObj,
+      newNoteBl:this.newNote,
+      showNoteMode: true,
+    }
+  },
+  methods:{
+    editNote: function () {
+      this.showNoteMode = this.newNoteBl = false
+    },
+    delNote:function(){
+      this.$el.style.opacity = '0';
+      setTimeout(function (scope) {
+        scope.$emit('del-note', scope.noteObject)
+      }, 300, this);
+    },
+    saveNote: function () {
+      this.showNoteMode = this.newNoteBl = true
+      this.$emit('edit-note', this.noteObject)
+    },
+  }
+};
+</script>
+
